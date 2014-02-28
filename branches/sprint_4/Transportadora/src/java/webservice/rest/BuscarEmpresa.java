cd935fcd6cb1ac79579e4a63aac88241e308012c
lecmsena <lecmsena@gmail.com>
2014-02-28 14:37:28 +0000

    Atualização dos itens efetuados.

---
+++ branches/sprint_4/Transportadora/src/java/webservice/rest/BuscarEmpresa.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webservice.rest;
+
+import fachada.EmpresaFachada;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import modelo.Empresa;
+
+/**
+ * REST Web Service
+ *
+ * @author lesena
+ */
+@Path("generic")
+@Stateless
+public class BuscarEmpresa {
+
+    @Context
+    private UriInfo context;
+    @EJB
+    private EmpresaFachada fachada;
+    /**
+     * Creates a new instance of BuscarEmpresaPorEndereco
+     */
+    public BuscarEmpresa() {
+        
+    }
+
+    /**
+     * Retrieves representation of an instance of webservice.rest.BuscarEmpresa
+     * @return an instance of java.lang.String
+     */
+    @GET @Produces("application/json")
+    public List<Empresa> buscarTodas() {
+        List<Empresa> emp = fachada.listar();
+        return emp;
+    }
+
+}
