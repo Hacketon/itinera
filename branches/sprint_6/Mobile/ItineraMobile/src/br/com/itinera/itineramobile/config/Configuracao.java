b4378835f682c102b2b22b05cfce8bbeb5f363d0
chuckmar@gmail.com <chuckmar@gmail.com@814690d2-e2fe-7d75-e29b-56e2218c28d6>
2014-03-03 17:15:17 +0000

    Commit do projeto Mobile

---
+++ branches/sprint_6/Mobile/ItineraMobile/src/br/com/itinera/itineramobile/config/Configuracao.java
@@ -0,0 +1,9 @@
+package br.com.itinera.itineramobile.config;
+
+public class Configuracao {
+	
+	public static final String URL_BASE = "http://192.168.0.105:8080/Transportadora/webresources/";
+	
+	public static final String LOGAR = "usuario/login/";
+
+}
