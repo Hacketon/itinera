9112b415da9bbe8fcede962847db50b0cf969042
lecmsena <lecmsena@gmail.com>
2014-03-20 06:33:28 +0000

    Resolução: Ao preencher o campo categoria veículo é necessário que quando selecionada a categoria, verifique se a categoria é reboque ou semi-reboque, e no caso de verdadeiro, os campos tipo combustivel, km atual e média km/litro não devem ser visíveis e possíveis de preenchimento, no caso contrário os campos deverão estar disponíveis.

--- branches/sprint_6/Itinera-EJB/src/main/java/br/com/itinera/persistencia/CategoriaVeiculoDAO.java
+++ branches/sprint_6/Itinera-EJB/src/main/java/br/com/itinera/persistencia/CategoriaVeiculoDAO.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import br.com.itinera.modelo.CategoriaVeiculo;
+import java.math.BigDecimal;
 
 /**
  *
@@ -16,7 +17,7 @@ public class CategoriaVeiculoDAO {
 
     @PersistenceContext
     private EntityManager em;
-
+    
     public void inserir(CategoriaVeiculo novo) throws EntityExistsException, Exception {
         em.persist(novo);
     }
@@ -32,5 +33,10 @@ public class CategoriaVeiculoDAO {
 
     public List<CategoriaVeiculo> listar() {
         return em.createNamedQuery("CategoriaVeiculo.findAll").getResultList();
+    }
+    
+    public CategoriaVeiculo recuperarPorId(BigDecimal id){
+        CategoriaVeiculo cv = (CategoriaVeiculo) em.createNamedQuery("CategoriaVeiculo.findByIdCategoriaVeiculo").setParameter("idCategoriaVeiculo", id).getSingleResult();
+        return cv;
     }
 }
