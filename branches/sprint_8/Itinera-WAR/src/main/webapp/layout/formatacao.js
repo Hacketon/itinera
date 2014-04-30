9f1a53b134a7be4e40346f7cc4ad3e911c497184
lecmsena <lecmsena@gmail.com>
2014-04-30 23:03:12 +0000

    [BUG] Campo de telefone sem máscara e aceitando qualquer caractere. Resolução: Adicionado formataTelefone no script js de formatação e utilizado no campo. Também centralizei os campos e troquei o botão de excluir pelo padrão já criado de texto com underline (commandlink). Adicionei formataNumero no campo de ramal.

--- branches/sprint_8/Itinera-WAR/src/main/webapp/layout/formatacao.js
+++ branches/sprint_8/Itinera-WAR/src/main/webapp/layout/formatacao.js
@@ -48,3 +48,25 @@ function formatarKm(v) {
     return v;
 }
 
+function formataTelefone(v){
+    
+    var r = v.replace(/\D/g,"");
+    r = r.replace(/^0/,"");
+    if (r.length > 10) {
+        // 11+ digits. Format as 5+4.
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/,"(0$1)$2-$3");
+    }
+    else if (r.length > 5) {
+        // 6..10 digits. Format as 4+4
+        r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/,"(0$1)$2-$3");
+    }
+    else if (r.length > 2) {
+        // 3..5 digits. Add (0XX..)
+        r = r.replace(/^(\d\d)(\d{0,5})/,"(0$1)$2");
+    }
+    else {
+        // 0..2 digits. Just add (0XX
+        r = r.replace(/^(\d*)/, "(0$1");
+    }
+    return r;
+}
