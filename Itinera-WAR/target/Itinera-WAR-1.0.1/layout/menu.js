4ece40b90349d735a6e8e58399c6be8a42553152
brunoaccastro <brunoaccastro@gmail.com>
2014-05-27 00:50:12 +0000

    NÃO COMITAR A PASTA TARGET NOVAMENTE Exclusão da pasta target do repositório, essa pasta só serve localmente para subirmos a aplicação no glassfish local.

--- Itinera-WAR/target/Itinera-WAR-1.0.1/layout/menu.js
+++
@@ -1,9 +0,0 @@
-
-// seta os botões com o css correto, ao seleciona-lo.
-function selecionado(botao){
-    
-        $(botao).removeClass('botaomenu');
-        $(botao).addClass('botaomenuselecionado');
-    
-};
-
