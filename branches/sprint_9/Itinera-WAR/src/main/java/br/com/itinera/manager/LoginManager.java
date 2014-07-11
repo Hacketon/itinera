0057e31438db4ce5188c5244f0a8327ea47f7746
lecmsena <lecmsena@gmail.com>
2014-07-11 02:18:19 +0000

    Alteração de arquivos para funcionamento correto do login de usuários e segurança

--- branches/sprint_9/Itinera-WAR/src/main/java/br/com/itinera/manager/LoginManager.java
+++ branches/sprint_9/Itinera-WAR/src/main/java/br/com/itinera/manager/LoginManager.java
@@ -5,15 +5,15 @@
 package br.com.itinera.manager;
 
 import br.com.itinera.fachada.UsuarioFachada;
-import br.com.itinera.ferramentas.Encrypt;
 import java.io.Serializable;
 import java.util.Date;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 import br.com.itinera.modelo.Usuario;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -28,6 +28,20 @@ public class LoginManager implements Serializable{
     private Date dataHoraEntrou;
     private boolean logado = false;
     private String mensagem;
+    private boolean grupoAdministrador;
+    private boolean grupoSecretaria;
+    private boolean grupoGestor;
+    
+    @EJB
+    private UsuarioFachada fachada;
+    
+    public void init(){
+        this.login = "";
+        this.senha = "";
+        this.grupoAdministrador = false;
+        this.grupoGestor = false;
+        this.grupoSecretaria = false;
+    }
     
     public String getMensagem() {
         return mensagem;
@@ -36,15 +50,6 @@ public class LoginManager implements Serializable{
     public void setMensagem(String mensagem) {
         this.mensagem = mensagem;
     }
-    
-    
-   @EJB
-   private UsuarioFachada fachada;
-   
-   public void init(){
-       login = "";
-       senha = "";
-   }
     
     public String getLogin() {
         return login;
@@ -77,13 +82,38 @@ public class LoginManager implements Serializable{
     public void setDataHoraEntrou(Date dataHoraEntrou) {
         this.dataHoraEntrou = dataHoraEntrou;
     }
+
+    public boolean isGrupoAdministrador() {
+        return grupoAdministrador;
+    }
+
+    public void setGrupoAdministrador(boolean grupoAdministrador) {
+        this.grupoAdministrador = grupoAdministrador;
+    }
+
+    public boolean isGrupoSecretaria() {
+        return grupoSecretaria;
+    }
+
+    public void setGrupoSecretaria(boolean grupoSecretaria) {
+        this.grupoSecretaria = grupoSecretaria;
+    }
+
+    public boolean isGrupoGestor() {
+        return grupoGestor;
+    }
+
+    public void setGrupoGestor(boolean grupoGestor) {
+        this.grupoGestor = grupoGestor;
+    }
+    
    
     public String abrir(){
         return (this.logado)?"/layout/logado/index.xhtml":"/layout/logoff/index.xhtml";
     }
     
-    public String entrar(){
-        
+    //Metodo para login
+    public String fazerLogin(){
         if(this.login.isEmpty()){
             mensagem = "Por favor, insira um login para entrar.";
             return "";
@@ -94,55 +124,56 @@ public class LoginManager implements Serializable{
                 return "";
             }
             else{
-                //Recuperando usuário
-                Usuario usuarioRecuperado = fachada.buscarPorLogin(login.toUpperCase());
-                if(usuarioRecuperado == null){
-                    mensagem="Login para usuário inválido.";
-                    return "";
-                }else{
-                    if(!usuarioRecuperado.getAtivo()){
-                        mensagem = "Usuário inativo";
-                        return "";
+                FacesContext contexto = FacesContext.getCurrentInstance();
+                HttpServletRequest request = (HttpServletRequest)contexto.getExternalContext().getRequest();
+                try{
+                    request.login(login, senha);
+                    this.usuario = fachada.buscarPorLogin(login);
+                    this.logado = true;
+                    this.dataHoraEntrou = new Date();
+                    this.mensagem = "";
+                    System.out.println("LOGOU");
+                    if(request.isUserInRole("Administrador")){
+                        System.out.println("Administrador");
+                        grupoAdministrador=true;
+                        grupoGestor = true;
+                        grupoSecretaria = true;
                     }
-                    else{
-                        criptografar();
-                        if(!(senha.compareTo(usuarioRecuperado.getSenha())==0)){
-                            mensagem = "Senha para usuário incorreta.";
-                            return "";
-                        }else{
-                            usuario = usuarioRecuperado;
-                            this.logado = true;
-                            this.dataHoraEntrou = new Date();
-                            return "/layout/logado/index";
-                        }
+                    else if(request.isUserInRole("Gestor")){
+                        grupoGestor=true;
+                        System.out.println("Gestor");
                     }
+                    else if(request.isUserInRole("Secretaria")){
+                        System.out.println("Secretária");
+                        grupoSecretaria=true;
+                    }
+                    else if(request.isUserInRole("Motorista"))System.out.println("Pertence à regra Motorista");
+                    
+                    return "/layout/logado/index.xhtml";
+                }catch(Exception e){
+                    mensagem = "Usuário ou Senha inválida.";
+                    return null;
                 }
                 
-                    
             }
         }
     }
     
-    public String sair(){
+    //Efetuar logout
+    public String fazerLogout(){
         this.logado = false;
         usuario = null;
-        HttpSession sessao = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-        sessao.invalidate();
+        this.init();
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest)contexto.getExternalContext().getRequest();
+        try {
+            request.logout();
+        } catch (ServletException ex) {
+            System.out.println(ex.getCause());
+        }
         return "/index.xhtml?faces-redirect=true";
     }
     
-     private boolean criptografar() {
-        Encrypt encriptador = new Encrypt(senha,"MD5","UTF-8");
-        boolean deuCerto = false;
-        if(encriptador.hasError()){
-            mensagem = "Erro ao criptografar.";
-        }
-        else{
-            senha = encriptador.getResult();
-            deuCerto = true;
-        }
-        return deuCerto;
-    }
     
 
  
