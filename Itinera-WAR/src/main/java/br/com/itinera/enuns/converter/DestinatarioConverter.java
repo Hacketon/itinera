85a704dbb62ec6d96c8b5cc1c2e5039b32e7cb81
lecmsena <lecmsena@gmail.com>
2014-07-15 11:07:07 +0000

    Resolução: Criar jsf (não funcional) referente à tela de inserção e alteração de ordem de coleta - Criação das classes para criação dos converters necessários para o funcionamento da tela. - Atualização do ManagedBean de Ordem de Coleta

---
+++ Itinera-WAR/src/main/java/br/com/itinera/enuns/converter/DestinatarioConverter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.itinera.enuns.converter;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author lesena
+ */
+@FacesConverter(value="conversorDestinatario")
+@SessionScoped
+@ManagedBean(name = "mngDestinatarioConverter")
+public class DestinatarioConverter implements Converter{
+    //TODO Implementar Converter
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
