31effb7fea1a5a6e2811f924098517e331d81661
lecmsena <lecmsena@gmail.com>
2014-07-16 00:01:53 +0000

    Resolução: Criar converter referente à busca de empresas do tipo usina (remetente) Criar converter referente à busca de empresas do tipo usina e clientes (destinatário) Alterar jsf referente à tela de inserção e alteração para funcionamento correto do remetente Alterar jsf referente à tela de inserção e alteração para funcionamento correto do destinatário Alterar jsf referente à tela de inserção e alteração paera funcionamento correto do campo de rota (concatenação da cidade do remetente com um traço e cidade do destinatário) Ex: Ribeirão Preto - São Carlos Alterar jsf refente à tela de inserção e alteração para funcionamento correto do calculo automático de quantidade x valor_unitário.

--- branches/sprint_9/Itinera-WAR/src/main/java/br/com/itinera/enuns/converter/VeiculoConverter.java
+++ branches/sprint_9/Itinera-WAR/src/main/java/br/com/itinera/enuns/converter/VeiculoConverter.java
@@ -6,6 +6,7 @@
 
 package br.com.itinera.enuns.converter;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
@@ -20,7 +21,7 @@ import javax.faces.convert.FacesConverter;
 @FacesConverter(value="conversorVeiculo")
 @SessionScoped
 @ManagedBean(name = "mngVeiculoConverter")
-public class VeiculoConverter implements Converter{
+public class VeiculoConverter implements Converter,Serializable{
     //TODO Implementar Converter
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
